ac4ee0da3a280dafafedc0eb2da2f9470331c019
Floris Kraak <fkraak@ymor.nl>
2016-05-27 11:37:43 +0200

    Create AnomalyDetectionRDECwrapper() by splitting AnomalyDetectionInRDECCSV() into two.
    We don't want to read anomaly data from file when calling this stuff from within RDEC. Instead we create the data directly.
    So, split out the bits we no longer need from the bits we (still) do need..

--- R/ymonitor_integration.R
+++ R/ymonitor_integration.R
@@ -14,6 +14,19 @@ AnomalyDetectionInRDECCSV <- function(input_csv = 'example.csv', max_anoms = 0.1
 	## Read datafile (downloaded from RDEC)
 	inputdata <- read.table(input_csv, header = TRUE, sep = "," , dec = "." , colClasses = "character", comment.char = "")
 	
+	return ( AnomalyDetectionRDECwrapper(inputdata, max_anoms, direction, alpha, only_last, threshold, e_value, longterm, piecewise_median_period_weeks, plot, y_log, xlabel, ylabel, title, verbose, na.rm) )
+}
+
+
+	
+AnomalyDetectionRDECwrapper <- function(inputdata, max_anoms = 0.10, direction = 'pos',
+                               alpha = 0.05, only_last = NULL, threshold = 'None',
+                               e_value = FALSE, longterm = FALSE, piecewise_median_period_weeks = 2, plot = FALSE,
+                               y_log = FALSE, xlabel = '', ylabel = 'count',
+                               title = NULL, verbose=FALSE, na.rm = FALSE){
+
+	print(inputdata$class);
+	
 	## Rename columns
 	names(inputdata)[names(inputdata)=="Date"] <- "timestamp"
 	names(inputdata)[names(inputdata)=="Duration"] <- "count"
